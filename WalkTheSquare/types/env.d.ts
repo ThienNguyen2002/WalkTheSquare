d185b92781ffe753f2a4ac99f90d3323b898feac
Thien Nguyen <thien.x.nguyen20@gmail.com>
2023-05-19 22:55:34 -0600

    Environment variables set up

---
+++ WalkTheSquare/types/env.d.ts
@@ -0,0 +1,10 @@
+declare module '@env'{
+    export const REACT_APP_APIKEY: string;
+    export const REACT_APP_AUTHDOMAIN: string;
+    export const REACT_APP_DB: string;
+    export const REACT_APP_PID: string;
+    export const REACT_APP_SB: string;
+    export const REACT_APP_SID: string;
+    export const REACT_APP_APPID: string;
+    export const REACT_APP_MID: string;
+}
