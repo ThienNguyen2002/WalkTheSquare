472d877b1e16a3bba66571f59288a64b5d5f8b52
Thien Nguyen <nguthien2002@gmail.com>
2023-05-22 22:12:42 -0600

    Added type

--- WalkTheSquare/App.tsx
+++ WalkTheSquare/App.tsx
@@ -1,6 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, ParamListBase } from '@react-navigation/native';
+import { createNativeStackNavigator, } from '@react-navigation/native-stack';
+import { StackNavigationProp } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Home: undefined;
+};
+
+export type RootStackNavigationProp = StackNavigationProp<RootStackParamList, keyof RootStackParamList>;
 
 
 const Stack = createNativeStackNavigator();
@@ -9,6 +18,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
       );
